4a323dbc709b596d2177f0977d638a2062aa13e7
fusuya <kaibutsu_oujo@excite.co.jp>
2018-10-08 16:44:04 +0900

    口パク（バグあり）

--- vpeer.lisp
+++ vpeer.lisp
@@ -11,15 +11,58 @@
 (defparameter *freq* 22050)
 (defparameter *cap_size* 2048)
 
+
+
+
+(defun load-img (img)
+  (gtk-image-new-from-pixbuf (gdk-pixbuf-new-from-file img)))
+
+(defstruct imgs
+  (img nil)
+  (body (load-img "./img/body.png"))
+  (open-mouth (load-img "./img/open-mouth.png"))
+  (close-mouth (load-img "./img/close-mouth.png"))
+  (open-eye (load-img "./img/open-eye.png"))
+  (close-eye (load-img "./img/close-eye.png")))
+
+(defstruct draw
+  (body nil)
+  (eye nil)
+  (mouth nil))
+
+(defun re-draw (new-img moge func frame ol)
+  (gtk-widget-destroy (funcall func moge))
+  (funcall (fdefinition `(setf ,func)) new-img moge)
+  (gtk-container-add frame (funcall func moge))
+  (gtk-overlay-add-overlay ol (funcall func moge))
+  (gtk-widget-show (funcall func moge))
+  )
+
+(defun mabataki (imgs moge frame ol)
+  (re-draw (imgs-close-eye imgs) moge 'draw-eye frame ol)
+  (g-timeout-add
+   200
+   (lambda ()
+     (re-draw (imgs-open-eye imgs) moge 'draw-eye frame ol)
+     nil)))
+
+(defun kuchipaku (imgs moge frame ol)
+  (re-draw (imgs-open-mouth imgs) moge 'draw-mouth frame ol)
+  (g-timeout-add
+   200
+   (lambda ()
+     (re-draw (imgs-close-mouth imgs) moge 'draw-mouth frame ol)
+     nil)))
+
 ;;音声再生部分いらない
-(defun altest2 (imgs frame window)
+(defun altest2 (imgs moge frame ol)
   ;;(print 7)
   (let* ((bufq nil)
          (error-code 0)
          (hellobuf (cffi:foreign-alloc :uint :count 16))
          (hellosource (cffi:foreign-alloc :uint))
          (audiodev nil) (audiocontext nil)
-         (inputdev nil)
+         (inputdev nil) (state nil)
          (buffer nil) (samplesin nil) (availbuffers nil)
          (mybuff nil) (bufferholder nil))
     ;;(print 0)
@@ -47,6 +90,7 @@
           samplesin (cffi:foreign-alloc :int :initial-element 0)
           availbuffers (cffi:foreign-alloc :int :initial-element 0)
           mybuff (cffi:foreign-alloc :uint)
+          state (cffi:foreign-alloc :int :initial-element 0)
           bufferholder (cffi:foreign-alloc :uint :count 16))
     ;;(print 3)
     (loop  while (null *end*)
@@ -58,24 +102,26 @@
               (%alc:get-integer-v inputdev :capture-samples 1 samplesin)
               (when (> (mem-ref samplesin :int) *cap_size*)
                 (%alc:capture-samples inputdev buffer *cap_size*)
-                (when (find-if #'(lambda (x) (>= (abs x) 200))
-                               (loop for i from 0 below *cap_size*
-                                     collect (mem-aref buffer :short i)))
-                  (mabataki imgs frame window))
-                (when bufq
-                  (let ((state (cffi:foreign-alloc :int :initial-element 0)))
-                    (setf (mem-ref mybuff :uint) (car bufq)
-                          bufq (cdr bufq))
-                    (al:buffer-data (mem-ref mybuff :uint) #x1101 buffer (* *cap_size* (cffi:foreign-type-size :short)) *freq*)
-                    ;;(when (= count 0)
-                    ;;  (format t "~s~%" (loop for i from 0 below *cap_size*
-                    ;;                         collect (mem-aref buffer :short i))))
-                    (%al:source-queue-buffers (mem-aref hellosource :uint 0) 1 mybuff)
-                    (%al:get-source-i (mem-aref hellosource :uint 0) :source-state state)
-                    (when (/= (mem-ref state :int) #x1012)
-                      (al:source-play (mem-aref hellosource :uint 0)))
-                    ;;(incf count)
-                    (cffi:foreign-free state))))
+                (when (and (find-if #'(lambda (x) (>= (abs x) 300))
+                                    (loop for i from 0 below *cap_size*
+                                          collect (mem-aref buffer :short i)))
+                           (equal (draw-mouth moge) (imgs-close-mouth imgs)))
+                  (kuchipaku imgs moge frame ol)
+                  )
+                ;; (when bufq
+                ;;   (setf (mem-ref mybuff :uint) (car bufq)
+                ;;         bufq (cdr bufq))
+                ;;   (al:buffer-data (mem-ref mybuff :uint) #x1101 buffer (* *cap_size* (cffi:foreign-type-size :short)) *freq*)
+                ;;   ;;(when (= count 0)
+                ;;   ;;  (format t "~s~%" (loop for i from 0 below *cap_size*
+                ;;   ;;                         collect (mem-aref buffer :short i))))
+                ;;   (%al:source-queue-buffers (mem-aref hellosource :uint 0) 1 mybuff)
+                ;;   (%al:get-source-i (mem-aref hellosource :uint 0) :source-state state)
+                ;;   (when (/= (mem-ref state :int) #x1012)
+                ;;     (al:source-play (mem-aref hellosource :uint 0)))
+                ;;   ;;(incf count)
+                ;;   )
+                )
               (gtk-main-iteration-do nil))
 
     (cffi:foreign-free buffer)
@@ -101,73 +147,61 @@
     (alc:close-device audiodev)))
 
 
-
-(defstruct imgs
-  (img nil)
-  (defo (gdk-pixbuf-new-from-file "./img/neko.png"))
-  (close-eye (gdk-pixbuf-new-from-file "./img/neko-close-eye.png")))
-
-(defun re-draw (new-img imgs frame window)
-  (gtk-widget-destroy (imgs-img imgs))
-  ;;(gtk-container-remove frame (imgs-img imgs))
-  (setf (imgs-img imgs) (gtk-image-new-from-pixbuf new-img))
-  (gtk-container-add frame (imgs-img imgs))
-  (gtk-widget-show (imgs-img imgs))
-  ;;(gtk-widget-show-all window)
-  )
-
-(defun mabataki (imgs frame window)
-  (re-draw (imgs-close-eye imgs) imgs frame window)
-  (g-timeout-add
-   200
-   (lambda ()
-     (re-draw (imgs-defo imgs) imgs frame window)
-     nil)))
-
 (defun main ()
   (setf *random-state* (make-random-state t))
   (within-main-loop
     (let* ((window (gtk-window-new :toplevel))
-           (frame (make-instance 'gtk-frame :shadow-type :in))
-           (imgs (make-imgs)))
-      (setf (imgs-img imgs) (gtk-image-new-from-pixbuf (imgs-defo imgs)))
+           (body-f (make-instance 'gtk-frame :shadow-type :in :app-paintable t))
+           (eye-f (make-instance 'gtk-frame :shadow-type :in :app-paintable t))
+           (mouth-f (make-instance 'gtk-frame :shadow-type :in :app-paintable t))
+           (ol (make-instance 'gtk-overlay :app-paintable t))
+           (imgs (make-imgs))
+           (moge (make-draw :body (imgs-body imgs)
+                            :eye (imgs-open-eye imgs)
+                            :mouth (imgs-close-mouth imgs))))
       ;; Signal handler for the window to handle the signal "destroy".
       (gtk-window-resize window 480 320)
       (setf (gtk-window-title window) "vpeer")
       (setf (gtk-widget-app-paintable window) t)
       ;;(setf (gtk-window-decorated window) nil)
-      (let* ((screen (gtk-widget-get-screen frame))
+      (g-signal-connect window "destroy"
+                        (lambda (widget)
+                          (declare (ignore widget))
+                          ;;(gtk-widget-destroy window)
+                          (leave-gtk-main)))
+      (g-signal-connect window "key-press-event"
+                        (lambda (widget key)
+                          (declare (ignore widget))
+                          (let ((hoge (gdk-event-key-keyval key)))
+                            (cond
+                              ((= (char-code #\q) hoge)
+                               (setf *end* t) ;;test
+                               ;;(mabataki imgs moge eye-f ol)
+                               (if (gtk-window-decorated window)
+                                   (setf (gtk-window-decorated window) nil)
+                                   (setf (gtk-window-decorated window) t)))
+                              ((= (char-code #\s) hoge)
+                               (setf *end* nil)
+                               ;;(mabataki imgs moge eye-f ol)
+                               (altest2 imgs moge mouth-f ol))))))
+      (g-timeout-add
+       3000
+       (lambda ()
+         (if (> 3 (random 4))
+             (mabataki imgs moge eye-f ol))
+         t))
+      (let* ((screen (gtk-widget-get-screen  window))
              (visual (gdk-screen-get-rgba-visual screen)))
         (gtk-widget-set-visual window visual)
-        (g-signal-connect window "destroy"
-                          (lambda (widget)
-                            (declare (ignore widget))
-                            ;;(gtk-widget-destroy window)
-                            (leave-gtk-main)))
-        (g-signal-connect window "key-press-event"
-                          (lambda (widget key)
-                            (declare (ignore widget))
-                            (let ((hoge (gdk-event-key-keyval key)))
-                              (cond
-                                ((= (char-code #\q) hoge)
-                                 (setf *end* t) ;;test
-                                 (mabataki imgs frame window)
-                                 (if (gtk-window-decorated window)
-                                     (setf (gtk-window-decorated window) nil)
-                                     (setf (gtk-window-decorated window) t)))
-                                ((= (char-code #\s) hoge)
-                                 (setf *end* nil)
-                                 (mabataki imgs frame window)
-                                 (altest2 imgs frame window))))))
-        ;; (g-timeout-add
-        ;;  3000
-        ;;  (lambda ()
-        ;;    (if (> 3 (random 4))
-        ;;        (mabataki imgs frame window))
-        ;;    t))
 
-        (gtk-container-add frame (imgs-img imgs))
-        (gtk-container-add window frame)
+
+        (gtk-container-add body-f (draw-body moge))
+        (gtk-container-add eye-f (draw-eye moge))
+        (gtk-container-add mouth-f (draw-mouth moge))
+        (gtk-overlay-add-overlay ol body-f)
+        (gtk-overlay-add-overlay ol eye-f)
+        (gtk-overlay-add-overlay ol mouth-f)
+        (gtk-container-add window ol)
         ;; Show the window.
         (gtk-widget-show-all window))
       ;;(altest2 imgs frame window)
